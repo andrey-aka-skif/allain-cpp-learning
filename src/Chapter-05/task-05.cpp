cbf3739e8ded3516ad665543a23937223dacdcab
Andrey <2119163@gmail.com>
2025-06-23 16:38:45 +0300

    learning (chapter-05): выполнено задание 6
    * Задание 5 пропущено

---
+++ src/Chapter-05/task-05.cpp
@@ -0,0 +1,19 @@
+/*
+* Alex Allain. Jumping into c++.
+* Глава 5. Циклы
+*
+* 5. Решите эти задачи, поочередно используя все типы циклов; определите,
+* какие циклы наилучшим образом подходят для каждой из задач.
+*/
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+
+	cout << "Пропускаем задание" << endl;
+}
