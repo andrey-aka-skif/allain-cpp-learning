a694107846c3fd2b23b909e79f2326d066884d62
Andrey <2119163@gmail.com>
2025-07-21 15:59:42 +0300

    refactor (chapter-08): унификация и модульность

--- src/Chapter-08/task-02.cpp
+++ src/Chapter-08/task-02.cpp
@@ -13,74 +13,121 @@
 #include <Windows.h>
 #include <limits>
 
-void run_number_guesser();
-int get_rnd_value(int low, int high);
-int int_cin();
+namespace CustomInput {
+	static int int_cin() {
+		int input = 0;
+		std::cin >> input;
+
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			throw std::runtime_error("Ошибка: введено не целое число");
+		}
+
+		return input;
+	}
+}
+
+namespace NumberGuesserGame {
+	int const DEFAULT_LOW_BOUND = 1;
+	int const DEFAULT_HIGH_BOUND = 100;
+
+	enum Result { UNKNOWN, NUMBER_IS_LESS_THEN_SECRET, NUMBER_IS_MORE_THEN_SECRET, EQUALS };
+
+	int secret_number = 0;
+	int low_bound = DEFAULT_LOW_BOUND;
+	int high_bound = DEFAULT_HIGH_BOUND;
+
+	static int get_rnd_value(int low, int high) {
+		return rand() % (high - low + 1) + low;
+	}
+
+	static void init_game(int low = DEFAULT_LOW_BOUND, int high = DEFAULT_HIGH_BOUND) {
+		low_bound = low;
+		high_bound = high;
+
+		srand(time(NULL));
+		secret_number = get_rnd_value(DEFAULT_LOW_BOUND, DEFAULT_HIGH_BOUND);
+	}
+
+	static Result get_guess_result(int guess) {
+		if (guess < secret_number)
+			return NUMBER_IS_LESS_THEN_SECRET;
+		else if (guess > secret_number)
+			return NUMBER_IS_MORE_THEN_SECRET;
+		else
+			return EQUALS;
+	}
+
+	static void run_number_guesser_game() {
+		NumberGuesserGame::init_game();
+
+		std::cout
+			<< "Угадайте число от "
+			<< DEFAULT_LOW_BOUND
+			<< " до "
+			<< DEFAULT_HIGH_BOUND
+			<< ":"
+			<< std::endl;
+
+		Result result = UNKNOWN;
+		int user_guess_number = 0;
+		int attempt_count = 0;
+
+		do
+		{
+			std::cout
+				<< "Вы думаете, что загаданное число: ";
+
+			try
+			{
+				user_guess_number = CustomInput::int_cin();
+				result = get_guess_result(user_guess_number);
+				attempt_count++;
+			}
+			catch (const std::exception&)
+			{
+				std::cout
+					<< "Ошибка ввода. Нужно число. Попробуйте ещё раз."
+					<< std::endl;
+				continue;
+			}
+
+			if (result == NUMBER_IS_LESS_THEN_SECRET) {
+				std::cout
+					<< "Слишком мало! (число попыток: "
+					<< attempt_count
+					<< ")."
+					<< std::endl;
+			}
+			else if (result == NUMBER_IS_MORE_THEN_SECRET) {
+				std::cout
+					<< "Слишком много! (число попыток: "
+					<< attempt_count
+					<< ")."
+					<< std::endl;
+			}
+		} while (result != EQUALS);
+
+		std::cout
+			<< "Вы угадали число "
+			<< NumberGuesserGame::secret_number
+			<< " за "
+			<< attempt_count
+			<< " попыток."
+			<< std::endl;
+	}
+}
 
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
 
 	try
 	{
-		run_number_guesser();
+		NumberGuesserGame::run_number_guesser_game();
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
-}
-
-void run_number_guesser() {
-	srand(time(NULL));
-
-	int const LOW = 1;
-	int const HIGH = 100;
-
-	int hidden_number = get_rnd_value(LOW, HIGH);
-	int user_number = 0;
-	int attempts_number = 0;
-
-	std::cout << "Угадайте число от " << LOW << " до " << HIGH << ":" << std::endl;
-
-	while (user_number != hidden_number)
-	{
-		std::cout << "Вы думаете, что загаданное число: ";
-
-		try
-		{
-			user_number = int_cin();
-			attempts_number++;
-		}
-		catch (const std::exception&)
-		{
-			std::cout << "Ошибка ввода. Нужно число. Попробуйте ещё раз." << std::endl;
-			continue;
-		}
-
-		if (user_number < hidden_number) {
-			std::cout << "Слишком мало! (число попыток: " << attempts_number << ")." << std::endl;
-		}
-		else {
-			std::cout << "Слишком много! (число попыток: " << attempts_number << ")." << std::endl;
-		}
-	}
-
-	std::cout << "Вы угадали число " << hidden_number << " за " << attempts_number << " попыток." << std::endl;
-}
-
-int get_rnd_value(int low, int high) {
-	return rand() % (high - low + 1) + low;
-}
-
-int int_cin() {
-	int input = 0;
-	std::cin >> input;
-
-	if (std::cin.fail()) {
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		throw std::runtime_error("Ошибка: введено не целое число");
-	}
-
-	return input;
 }
