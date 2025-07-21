778e12b89cd1eba5e3e9651e9eb9fbbf158ca13c
andrey-aka-skif <2119163@gmail.com>
2025-07-21 18:18:45 +0300

    refactor (chapter-08): унификация и модульность task-03

--- src/Chapter-08/task-03.cpp
+++ src/Chapter-08/task-03.cpp
@@ -11,69 +11,161 @@
 #include <Windows.h>
 #include <ctime>
 
-enum Result { UNKNOWN, NEED_MORE, NEED_LESS, EQUALS };
+namespace CustomInput {
+	static int int_cin() {
+		int input = 0;
+		std::cin >> input;
 
-void run_number_guesser();
-Result get_guess_result(int guess, int secret_number);
-int get_rnd_value(int low, int high);
-int get_guess(int down_limit, int up_limit);
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			throw std::runtime_error("Ошибка: введено не целое число");
+		}
+
+		return input;
+	}
+}
+
+namespace NumberGuesserGame {
+	int const DEFAULT_LOW_BOUND = 1;
+	int const DEFAULT_HIGH_BOUND = 100;
+
+	enum Result { UNKNOWN, NUMBER_IS_LESS_THEN_SECRET, NUMBER_IS_MORE_THEN_SECRET, EQUALS };
+
+	int secret_number = 0;
+	int low_bound = DEFAULT_LOW_BOUND;
+	int high_bound = DEFAULT_HIGH_BOUND;
+
+	static int get_rnd_value(int low, int high) {
+		return rand() % (high - low + 1) + low;
+	}
+
+	static void init_game(int low = DEFAULT_LOW_BOUND, int high = DEFAULT_HIGH_BOUND) {
+		low_bound = low;
+		high_bound = high;
+
+		srand(time(NULL));
+		secret_number = get_rnd_value(DEFAULT_LOW_BOUND, DEFAULT_HIGH_BOUND);
+	}
+
+	static Result get_guess_result(int guess) {
+		if (guess < secret_number)
+			return NUMBER_IS_LESS_THEN_SECRET;
+		else if (guess > secret_number)
+			return NUMBER_IS_MORE_THEN_SECRET;
+		else
+			return EQUALS;
+	}
+
+	static void run_number_guesser_game() {
+		NumberGuesserGame::init_game();
+
+		std::cout
+			<< "Угадайте число от "
+			<< DEFAULT_LOW_BOUND
+			<< " до "
+			<< DEFAULT_HIGH_BOUND
+			<< ":"
+			<< std::endl;
+
+		Result result = UNKNOWN;
+		int user_guess_number = 0;
+		int attempt_count = 0;
+
+		do
+		{
+			std::cout
+				<< "Вы думаете, что загаданное число: ";
+
+			try
+			{
+				user_guess_number = CustomInput::int_cin();
+				result = get_guess_result(user_guess_number);
+				attempt_count++;
+			}
+			catch (const std::exception&)
+			{
+				std::cout
+					<< "Ошибка ввода. Нужно число. Попробуйте ещё раз."
+					<< std::endl;
+				continue;
+			}
+
+			if (result == NUMBER_IS_LESS_THEN_SECRET) {
+				std::cout
+					<< "Слишком мало! (число попыток: "
+					<< attempt_count
+					<< ")."
+					<< std::endl;
+			}
+			else if (result == NUMBER_IS_MORE_THEN_SECRET) {
+				std::cout
+					<< "Слишком много! (число попыток: "
+					<< attempt_count
+					<< ")."
+					<< std::endl;
+			}
+		} while (result != EQUALS);
+
+		std::cout
+			<< "Вы угадали число "
+			<< NumberGuesserGame::secret_number
+			<< " за "
+			<< attempt_count
+			<< " попыток."
+			<< std::endl;
+	}
+}
+
+namespace NumberPicker {
+	static int get_guess(int down_limit, int up_limit) {
+		return NumberGuesserGame::get_rnd_value(down_limit, up_limit);
+	}
+
+	static void run_number_picker() {
+		NumberGuesserGame::init_game();
+
+		std::cout
+			<< "Загаданное число: "
+			<< NumberGuesserGame::secret_number
+			<< std::endl;
+
+		NumberGuesserGame::Result result = NumberGuesserGame::UNKNOWN;
+		int guess = 0;
+		int attempt_count = 0;
+		int low_bound = NumberGuesserGame::DEFAULT_LOW_BOUND;
+		int high_bound = NumberGuesserGame::DEFAULT_HIGH_BOUND;
+
+		while (result != NumberGuesserGame::EQUALS)
+		{
+			guess = get_guess(low_bound, high_bound);
+			result = NumberGuesserGame::get_guess_result(guess);
+			attempt_count++;
+
+			std::cout
+				<< "Попытка № "
+				<< attempt_count
+				<< ": "
+				<< guess
+				<< std::endl;
+
+			if (result == NumberGuesserGame::NUMBER_IS_MORE_THEN_SECRET)
+				high_bound = guess - 1;
+			else if (result == NumberGuesserGame::NUMBER_IS_LESS_THEN_SECRET)
+				low_bound = guess + 1;
+		}
+	}
+}
 
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
 
 	try
 	{
-		run_number_guesser();
+		NumberPicker::run_number_picker();
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
-}
-
-void run_number_guesser() {
-	int const LOW = 1;
-	int const HIGH = 100;
-
-	srand(time(NULL));
-	int secret_number = get_rnd_value(LOW, HIGH);
-
-	std::cout << "Загаданное число: " << secret_number << std::endl;
-
-	Result result = UNKNOWN;
-	int guess = 0;
-	int attempts_number = 0;
-	int low_border = LOW;
-	int high_border = HIGH;
-
-	while (result != EQUALS)
-	{
-		guess = get_guess(low_border, high_border);
-		result = get_guess_result(guess, secret_number);
-		attempts_number++;
-
-		std::cout << "Попытка № " << attempts_number << ": " << guess << std::endl;
-
-		if (result == NEED_LESS)
-			low_border = guess + 1;
-		else if (result == NEED_MORE)
-			high_border = guess - 1;
-	}
-}
-
-Result get_guess_result(int guess, int secret_number) {
-	if (guess < secret_number)
-		return NEED_LESS;
-	else if (guess > secret_number)
-		return NEED_MORE;
-	else
-		return EQUALS;
-}
-
-int get_guess(int down_limit, int up_limit) {
-	return get_rnd_value(down_limit, up_limit);
-}
-
-int get_rnd_value(int low, int high) {
-	return rand() % (high - low + 1) + low;
 }
